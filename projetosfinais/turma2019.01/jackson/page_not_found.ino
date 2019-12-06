97ae3b05715235e2cb9d9922186eecafaac3bc0d
Jackson D <j.dantas27@gmail.com>
2019-12-06 13:14:24 -0300

    Final

---
+++ projetosfinais/turma2019.01/jackson/page_not_found.ino
@@ -0,0 +1,16 @@
+void handleNotFound() {
+  digitalWrite(indicator, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  digitalWrite(indicator, 0);
+}
